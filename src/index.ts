6d941092c42a6242f2afeb9e77e5807ea8c1fc81
Chuck Chen <chuck.j.chen@gmail.com>
2024-07-30 15:47:00 +0800

    Add worker AI binding

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,24 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
-
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+	async fetch(request, env) {
+	  const tasks = [];
+  
+	  // prompt - simple completion style input
+	  let simple = {
+		prompt: 'Tell me a joke about Cloudflare'
+	  };
+	  let response = await env.AI.run('@cf/meta/llama-3-8b-instruct', simple);
+	  tasks.push({ inputs: simple, response });
+  
+	  // messages - chat style input
+	  let chat = {
+		messages: [
+		  { role: 'system', content: 'You are a helpful assistant.' },
+		  { role: 'user', content: 'Who won the world series in 2020?' }
+		]
+	  };
+	  response = await env.AI.run('@cf/meta/llama-3-8b-instruct', chat);
+	  tasks.push({ inputs: chat, response });
+  
+	  return Response.json(tasks);
+	}
+  };
