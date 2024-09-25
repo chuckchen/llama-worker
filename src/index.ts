c9fe796c7dc15f2c6d21ab6e9498023c1ca38f7f
Chuck Chen <chuck.j.chen@gmail.com>
2024-09-25 15:50:26 +0800

    Add streaming chat completion API

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-export default {
-	async fetch(request, env) {
-	  const tasks = [];
-  
-	  // prompt - simple completion style input
-	  let simple = {
-		prompt: 'Tell me a joke about Cloudflare'
-	  };
-	  let response = await env.AI.run('@cf/meta/llama-3-8b-instruct', simple);
-	  tasks.push({ inputs: simple, response });
-  
-	  // messages - chat style input
-	  let chat = {
-		messages: [
-		  { role: 'system', content: 'You are a helpful assistant.' },
-		  { role: 'user', content: 'Who won the world series in 2020?' }
-		]
-	  };
-	  response = await env.AI.run('@cf/meta/llama-3-8b-instruct', chat);
-	  tasks.push({ inputs: chat, response });
-  
-	  return Response.json(tasks);
-	}
-  };
+import { Hono } from 'hono';
+import { stream, streamText, streamSSE } from 'hono/streaming';
+
+const app = new Hono<{ Bindings: Env }>();
+
+const defaultMessage = [
+	{ role: 'system', content: 'You are a friendly assistant' },
+	{
+		role: 'user',
+		content: 'What is the origin of the phrase Hello, World',
+	},
+];
+
+// Stream response that can be handled in browsers with EventSource.
+// cf: https://developers.cloudflare.com/workers-ai/models/llama-3.1-8b-instruct
+app.post('/v1/chat/completions', async (c) =>
+	streamSSE(c, async (stream) => {
+		const { model, messages } = await c.req.json();
+
+		const chatStream = await c.env.AI.run('@cf/meta/llama-3.1-8b-instruct', { messages: messages ?? defaultMessage, stream: true });
+
+		await stream.pipe(chatStream as ReadableStream);
+	})
+);
+
+export default { fetch: app.fetch } satisfies ExportedHandler<Env>;
